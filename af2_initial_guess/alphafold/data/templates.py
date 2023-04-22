c4967606d271b06871970a3cb18b1d1b14226d78
Nathaniel Bennett <nrbennet@uw.edu>
2023-04-22 10:23:30 -0700

    Add ability to fix residues in MPNN. Also fixed some deprecated types

--- af2_initial_guess/alphafold/data/templates.py
+++ af2_initial_guess/alphafold/data/templates.py
@@ -92,14 +92,14 @@ TEMPLATE_FEATURES = {
     'template_aatype': np.float32,
     'template_all_atom_masks': np.float32,
     'template_all_atom_positions': np.float32,
-    'template_domain_names': np.object,
+    'template_domain_names': object,
     'template_e_value': np.float32,
     'template_neff': np.float32,
     'template_prob_true': np.float32,
-    'template_release_date': np.object,
+    'template_release_date': object,
     'template_score': np.float32,
     'template_similarity': np.float32,
-    'template_sequence': np.object,
+    'template_sequence': object,
     'template_sum_probs': np.float32,
     'template_confidence_scores': np.int64
 }
