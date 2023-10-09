ee1e362f2252dfb2290c723e60afb1cce70e61a8
Nathaniel Bennett <nrbennet@uw.edu>
2023-10-09 14:35:11 -0700

    Add split conda envs and import tests. Add example inputs

--- include/importtests/af2_importtest.py
+++ include/importtests/af2_importtest.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
+'''
+    Test the imports required by the AF2 initial guess scripts
+
+    This will also test whether the currently installed version of JAX is able to 
+    run on the GPU.
+
+    Run this on a gpu node like this:
+    python importtest.py
+'''
+
 import os
 import mock
 import numpy as np
 import sys
+# Get the path of the script
+scriptdir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(f'{scriptdir}/../../af2_initial_guess')
+
 import datetime
 
 from typing import Any, Mapping, Optional, Sequence, Tuple
@@ -34,7 +48,13 @@ from alphafold.model import config
 from alphafold.model import model
 from alphafold.data.tools import hhsearch
 
+sys.path.append(f'{scriptdir}/../..')
 from include.silent_tools import silent_tools
 
 from jax.lib import xla_bridge
-print(xla_bridge.get_backend().platform)
+device = xla_bridge.get_backend().platform
+
+if device == 'gpu':
+    print('Found a GPU! This environment passes all import tests')
+else:
+    print('No GPU found! This environment passes all import tests, but will not be able to use a GPU!!')
